caedac7af42b3d02a7ad3729090f1d672a2446d1
HollyCc <599887757@qq.com>
2023-07-31 19:28:52 +0800

    fix(other): 修复小小的配置bug

--- .env-config.ts
+++ .env-config.ts
@@ -4,13 +4,13 @@ type ServiceEnv = Record<ServiceEnvType, ServiceEnvConfig>;
 /** 不同请求服务的环境配置 */
 const serviceEnv: ServiceEnv = {
   dev: {
-    url: 'http://localhost:8080'
+    url: 'http://t.lihaha.cn/api'
   },
   test: {
-    url: 'http://localhost:8080'
+    url: 'http://t.lihaha.cn/api'
   },
   prod: {
-    url: 'http://localhost:8080'
+    url: 'http://t.lihaha.cn/api'
   }
 };
 
