ebbfc2081541e41cf7c7095a9cb2cc00b9b9d395
lihawhaw <lihawhaw@163.com>
2024-01-01 23:40:39 +0800

    chore: fetch url

--- .env-config.ts
+++ .env-config.ts
@@ -5,14 +5,14 @@ type ServiceEnv = Record<ServiceEnvType, ServiceEnvConfig>;
 const serviceEnv: ServiceEnv = {
   dev: {
     // url: 'http://api.t.lihaha.cn/admin-api'
-    url: 'http://t.lihaha.cn/api/admin-api'
+    url: 'http://book.wocc.site/api/admin-api'
     // url: 'http://127.0.0.1:8849/admin-api'
   },
   test: {
-    url: 'http://t.lihaha.cn/api/admin-api'
+    url: 'http://book.wocc.site/api/admin-api'
   },
   prod: {
-    url: 'http://t.lihaha.cn/api/admin-api'
+    url: 'http://book.wocc.site/api/admin-api'
   }
 };
 
